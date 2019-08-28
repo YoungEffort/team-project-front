47d99018e3290b74d9a497defc8be13ea1a97ee0
秦国胜 <qin41588@gmail.com>
2019-08-28 13:06:16 +0800

    修改格式化

--- src/store/index.js
+++ src/store/index.js
@@ -5,30 +5,30 @@
 */
 
 // 仓库输出 整合 reducers // compose
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from 'redux';
 
 // 数据状态持久化
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 // redux 中间件
-import createSagaMiddleware from "redux-saga";
-import Sagas from "../store/sagas/index";
+import createSagaMiddleware from 'redux-saga';
+import Sagas from '../store/sagas/index';
 
 // 引入规则文件
-import Reducers from "./reducers/index.js";
+import Reducers from './reducers/index.js';
 
 //创建中间件
 const sagaMiddleware = createSagaMiddleware();
 
 // 需要缓存的状态
 const persistConfig = {
-  key: "root",
-  storage: storage,
-  stateReconciler: autoMergeLevel2, // 查看 'Merge Process' 部分的具体情况,
-  whitelist: [""] // 指定需要缓冲的数据，不指定将缓存所有 [ 'Home' ]
-  // blacklist: ['navigation']                // 指定不需要缓冲的数据，缓存黑名单
+   key: 'root',
+   storage: storage,
+   stateReconciler: autoMergeLevel2, // 查看 'Merge Process' 部分的具体情况,
+   whitelist: [ '' ] // 指定需要缓冲的数据，不指定将缓存所有 [ 'Home' ]
+   // blacklist: ['navigation']                // 指定不需要缓冲的数据，缓存黑名单
 };
 
 const myPersistReducer = persistReducer(persistConfig, Reducers);
