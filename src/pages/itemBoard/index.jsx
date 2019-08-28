98f6e2a0cec95cc9dca3d2744d9b6f8a17877b0d
秦国胜 <qin41588@gmail.com>
2019-08-28 09:17:58 +0800

    验证

--- src/pages/itemBoard/index.jsx
+++ src/pages/itemBoard/index.jsx
@@ -4,7 +4,7 @@
    2019-08-26
 */
 import React, { Component } from 'react'
-import { Tooltip, Card, Icon, Row, Col, Button } from 'antd'
+import { Tooltip, Card, Icon, Row, Button } from 'antd'
 import './style.less'
 import ItemSearch from './component/search' // 查询
 import AddCompileItem from './component/addCompileItem' // 新增
